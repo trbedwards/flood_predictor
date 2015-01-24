b3ddb5a6d73845b9dc48ff64c9064d93a9841467
Thomas Edwards <tom.rb.edwards@gmail.com>
2015-01-24 20:37:29 +0000

    Changed location to South-east of London

--- js/map.js
+++ js/map.js
@@ -1,16 +1,16 @@
 function initialize() {
-  var chicago = new google.maps.LatLng(41.875696,-87.624207);
+  var latlong = new google.maps.LatLng(51.347592, 0.353578);
   var mapOptions = {
     zoom: 11,
-    center: chicago
+    center: latlong
   }
 
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-  var ctaLayer = new google.maps.KmlLayer({
-    url: 'http://gmaps-samples.googlecode.com/svn/trunk/ggeoxml/cta.kml'
-  });
-  ctaLayer.setMap(map);
+  // var ctaLayer = new google.maps.KmlLayer({
+  //   url: 'http://gmaps-samples.googlecode.com/svn/trunk/ggeoxml/cta.kml'
+  // });
+  // ctaLayer.setMap(map);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
