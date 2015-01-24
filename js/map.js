904eb779872e6ebadc072986ec1f62ef7ab14688
Thomas Edwards <tom.rb.edwards@gmail.com>
2015-01-24 20:13:00 +0000

    Added transit layer

--- js/map.js
+++ js/map.js
@@ -1,9 +1,14 @@
 function initialize() {
+  var myLatlng = new google.maps.LatLng(51.5, -0.11);
   var mapOptions = {
-    center: { lat: 51.5, lng: -0.11},
     zoom: 13,
-  };
-  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
+    center: myLatlng
+  }
 
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var transitLayer = new google.maps.TransitLayer();
+  transitLayer.setMap(map);  
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
