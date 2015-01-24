116cf858853e95b234d22f0d0711ab43f425e421
Thomas Edwards <tom.rb.edwards@gmail.com>
2015-01-24 20:16:13 +0000

    Added KML layer to Chicago

--- js/map.js
+++ js/map.js
@@ -1,14 +1,16 @@
 function initialize() {
-  var myLatlng = new google.maps.LatLng(51.5, -0.11);
+  var chicago = new google.maps.LatLng(41.875696,-87.624207);
   var mapOptions = {
-    zoom: 13,
-    center: myLatlng
+    zoom: 11,
+    center: chicago
   }
 
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-  var transitLayer = new google.maps.TransitLayer();
-  transitLayer.setMap(map);  
+  var ctaLayer = new google.maps.KmlLayer({
+    url: 'http://gmaps-samples.googlecode.com/svn/trunk/ggeoxml/cta.kml'
+  });
+  ctaLayer.setMap(map);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
